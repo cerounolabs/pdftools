40f8442b84b861bf5709b7a21639d1ba06a235c9
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2019-02-11 21:43:19 -0300

    bug "Histórico de Lugares de Trabajo"

---
+++ src/pdftools/SendEmail.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdftools;
+
+/*
+ * @author CEROUNO Labs - Christian Zelaya
+ * @version 1.0
+ */
+
+public class SendEmail {
+    private final String email_host = "";
+    private final String email_to   = "";
+    private final String email_from = "";
+    
+    
+    public SendEmail() {
+    }
+    
+    
+}
